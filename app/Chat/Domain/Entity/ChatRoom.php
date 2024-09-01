019455e249f021fbd0df3233bc1e492a3e08c090
Juan Daniel Forner Garriga <jdanielforga@gmail.com>
2024-09-01 14:52:57 +0200

    doc: added phpdoc to ChatRoom entity

--- app/Chat/Domain/Entity/ChatRoom.php
+++ app/Chat/Domain/Entity/ChatRoom.php
@@ -11,6 +11,7 @@ use App\Chat\Domain\ValueObject\ChatRoomName;
 use App\Chat\Domain\ValueObject\MessageId;
 use App\Helpers\ValueObjectList;
 use App\Shared\Domain\Entity\Entity;
+use App\Shared\Domain\Exception\InvalidArgumentException;
 use App\Shared\Domain\Exception\LogicException;
 use App\Shared\Domain\Exception\MessageAlreadyInChatException;
 use App\Shared\Domain\Exception\UserAlreadyInChatRoomException;
@@ -26,24 +27,53 @@ final class ChatRoom extends Entity
     private ValueObjectList $messages;
     private UserId $creatorId;
 
+    /**
+     * Sets the chat room ID.
+     *
+     * @param string $id The ID to set.
+     * @return self
+     * @throws InvalidArgumentException If the provided ID is not valid.
+     */
     public function setId(string $id): self
     {
         $this->id = ChatRoomId::create($id);
         return $this;
     }
 
+    /**
+     * Sets the chat room name.
+     *
+     * @param string $name The name to set.
+     * @return self
+     * @throws InvalidArgumentException If the provided name is not valid.
+     */
     public function setName(string $name): self
     {
         $this->name = ChatRoomName::create($name);
         return $this;
     }
 
+    /**
+     * Sets the chat room description.
+     *
+     * @param string $description The description to set.
+     * @return self
+     * @throws InvalidArgumentException If the provided description is not valid.
+     */
     public function setDescription(string $description): self
     {
         $this->description = ChatRoomDescription::create($description);
         return $this;
     }
 
+    /**
+     * Sets the members of the chat room.
+     *
+     * @param array $members An array of member IDs.
+     * @return self
+     * @throws ChatRoomCannotBeEmptyException If the members array is empty.
+     * @throws InvalidArgumentException If a member ID is not valid.
+     */
     public function setMembers(array $members): self
     {
         if (count($members) === 0) {
@@ -52,13 +82,20 @@ final class ChatRoom extends Entity
 
         $this->members = new ValueObjectList();
 
-        $this->members->set(array_map(function (string $member) {
-            return UserId::create($member);
-        }, $members));
+        $this->members->set(array_map(
+            function (string $member) {
+                return UserId::create($member);
+            }, $members));
 
         return $this;
     }
 
+    /**
+     * Sets the messages of the chat room.
+     *
+     * @param array $messages An array of messages.
+     * @return self
+     */
     public function setMessages(array $messages): self
     {
         $this->messages = new ValueObjectList();
@@ -66,12 +103,25 @@ final class ChatRoom extends Entity
         return $this;
     }
 
+    /**
+     * Sets the creator ID of the chat room.
+     *
+     * @param string $creatorId The creator ID to set.
+     * @return self
+     * @throws InvalidArgumentException If the creator ID is not valid.
+     */
     public function setCreatorId(string $creatorId): self
     {
         $this->creatorId = UserId::create($creatorId);
         return $this;
     }
 
+    /**
+     * Gets the chat room ID.
+     *
+     * @return string The chat room ID.
+     * @throws LogicException If the ID is not set.
+     */
     public function getId(): string
     {
         if (!isset($this->id)) {
@@ -81,6 +131,12 @@ final class ChatRoom extends Entity
         return $this->id->value();
     }
 
+    /**
+     * Gets the chat room name.
+     *
+     * @return string The chat room name.
+     * @throws LogicException If the name is not set.
+     */
     public function getName(): string
     {
         if (!isset($this->name)) {
@@ -90,6 +146,12 @@ final class ChatRoom extends Entity
         return $this->name->value();
     }
 
+    /**
+     * Gets the chat room description.
+     *
+     * @return string The chat room description.
+     * @throws LogicException If the description is not set.
+     */
     public function getDescription(): string
     {
         if (!isset($this->description)) {
@@ -99,6 +161,12 @@ final class ChatRoom extends Entity
         return $this->description->value();
     }
 
+    /**
+     * Gets the members of the chat room.
+     *
+     * @return array An array of member IDs.
+     * @throws LogicException If the members are not set.
+     */
     public function getMembers(): array
     {
         if (!isset($this->members)) {
@@ -114,6 +182,12 @@ final class ChatRoom extends Entity
         return $members;
     }
 
+    /**
+     * Gets the number of members in the chat room.
+     *
+     * @return int The number of members.
+     * @throws LogicException If the members are not set.
+     */
     public function getMembersCount(): int
     {
         if (!isset($this->members)) {
@@ -123,6 +197,12 @@ final class ChatRoom extends Entity
         return $this->members->count();
     }
 
+    /**
+     * Gets the messages in the chat room.
+     *
+     * @return array An array of message IDs.
+     * @throws LogicException If the messages are not set.
+     */
     public function getMessages(): array
     {
         if (!isset($this->messages)) {
@@ -134,6 +214,12 @@ final class ChatRoom extends Entity
         });
     }
 
+    /**
+     * Checks if the chat room is deleted.
+     *
+     * @return bool True if the chat room is deleted, otherwise false.
+     * @throws LogicException If the deletion status is not set.
+     */
     public function isDeleted(): bool
     {
         if (!isset($this->deletedAt)) {
@@ -143,6 +229,14 @@ final class ChatRoom extends Entity
         return $this->deletedAt->value() !== null;
     }
 
+    /**
+     * Updates the name of the chat room.
+     *
+     * @param string $name The new name to set.
+     * @param bool $isBulkUpdate Whether this is part of a bulk update.
+     * @return self
+     * @throws InvalidArgumentException If the name is not valid.
+     */
     public function updateName(string $name, bool $isBulkUpdate = false): self
     {
         $this->setName($name);
@@ -156,6 +250,14 @@ final class ChatRoom extends Entity
         return $this;
     }
 
+    /**
+     * Updates the description of the chat room.
+     *
+     * @param string $description The new description to set.
+     * @param bool $isBulkUpdate Whether this is part of a bulk update.
+     * @return self
+     * @throws InvalidArgumentException If the description is not valid.
+     */
     public function updateDescription(string $description, bool $isBulkUpdate): self
     {
         $this->setDescription($description);
@@ -169,6 +271,13 @@ final class ChatRoom extends Entity
         return $this;
     }
 
+    /**
+     * Adds a member to the chat room.
+     *
+     * @param string $newMember The ID of the member to add.
+     * @throws LogicException If the members list is not set.
+     * @throws UserAlreadyInChatRoomException If the member is already in the chat room.
+     */
     public function addMember(string $newMember): void
     {
         if (!isset($this->members)) {
@@ -184,6 +293,13 @@ final class ChatRoom extends Entity
         // TODO generate event
     }
 
+    /**
+     * Removes a member from the chat room.
+     *
+     * @param string $member The ID of the member to remove.
+     * @throws LogicException If the members list is not set.
+     * @throws UserDoesNotPertainsToChatRoomException If the member is not in the chat room.
+     */
     public function removeMember(string $member): void
     {
         if (!isset($this->members)) {
@@ -199,6 +315,12 @@ final class ChatRoom extends Entity
         // TODO generate event
     }
 
+    /**
+     * Adds a message to the chat room.
+     *
+     * @param string $newMessage The ID of the message to add.
+     * @throws MessageAlreadyInChatException If the message is already in the chat room.
+     */
     public function addMessage(string $newMessage): void
     {
         if ($this->hasMessage($newMessage)) {
@@ -210,11 +332,23 @@ final class ChatRoom extends Entity
         // TODO generate event
     }
 
+    /**
+     * Checks if a member is in the chat room.
+     *
+     * @param string $member The ID of the member to check.
+     * @return bool True if the member is in the chat room, otherwise false.
+     */
     public function hasMember(string $member): bool
     {
         return $this->members->contains($member) || $this->creatorId->value() === $member;
     }
 
+    /**
+     * Checks if a message is in the chat room.
+     *
+     * @param string $message The ID of the message to check.
+     * @return bool True if the message is in the chat room, otherwise false.
+     */
     public function hasMessage(string $message): bool
     {
         return $this->messages->contains($message);
